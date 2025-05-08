213626b46dbe26909be476916dba66fd92b8064d
nishchithahc <nishchitha4020@gmail.com>
2025-05-08 14:04:21 +0530

    Initial Commit

---
+++ src/main/java/sixth/prgroam/App.java
@@ -0,0 +1,13 @@
+package sixth.prgroam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
